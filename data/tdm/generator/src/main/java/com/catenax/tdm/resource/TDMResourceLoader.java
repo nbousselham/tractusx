1950fdd7c3e511c0d985c6e2a7248767a8a978c0
ckabelin <christian.kabelin@ventum.de>
2021-09-30 14:35:46 +0200

    feature/CAX-12@CXMVP-114-TestDataGenerator: add template based testdata generation and improved docker files

--- data/tdm/generator/src/main/java/com/catenax/tdm/resource/TDMResourceLoader.java
+++ data/tdm/generator/src/main/java/com/catenax/tdm/resource/TDMResourceLoader.java
@@ -1,3 +1,6 @@
+/*
+ *
+ */
 package com.catenax.tdm.resource;
 
 import java.io.BufferedReader;
@@ -7,51 +10,64 @@ import java.io.InputStreamReader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class TDMResourceLoader.
+ */
 public class TDMResourceLoader {
-	
+
+	/** The Constant log. */
 	private static final Logger log = LoggerFactory.getLogger(TDMResourceLoader.class);
 
+	/**
+	 * Load resource.
+	 *
+	 * @param path the path
+	 * @return the input stream
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	public static InputStream loadResource(String path) throws IOException {
-        try {
-        	ResourceLoader loader = new DefaultResourceLoader();
-            Resource resource = loader.getResource("classpath:" + path);
-            
-            // new ClassPathResource(filename).getInputStream();
-            
-            // File file = resource.getFile(); //.getFile("classpath:" + path);
-            InputStream in = resource.getInputStream(); // FileInputStream(file);
-            return in;
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            throw e;
-        }
-    }
-	
-	public static String resourceToString(String path) {
-		String result = "";
-		
 		try {
-			InputStream in = loadResource(path);
-			 
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-	        StringBuilder out = new StringBuilder();
-	        String line;
-	        while ((line = reader.readLine()) != null) {
-	            out.append(line);
-	        }
-	        
-	        result = out.toString();
-		} catch (Exception e) {
-			log.error(e.getMessage());
-			e.printStackTrace();
+			final ResourceLoader loader = new DefaultResourceLoader();
+			final Resource resource = loader.getResource("classpath:" + path);
+
+			// new ClassPathResource(filename).getInputStream();
+
+			// File file = resource.getFile(); //.getFile("classpath:" + path);
+			final InputStream in = resource.getInputStream(); // FileInputStream(file);
+			return in;
+		} catch (final IOException e) {
+			log.debug(e.getMessage());
+			throw e;
 		}
-		
+	}
+
+	/**
+	 * Resource to string.
+	 *
+	 * @param path the path
+	 * @return the string
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	public static String resourceToString(String path) throws IOException {
+		String result = "";
+
+		final InputStream in = loadResource(path);
+
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		final StringBuilder out = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			out.append(line);
+		}
+
+		result = out.toString();
+
 		return result;
 	}
-	
+
 }
--- data/tdm/generator/src/main/java/io/swagger/api/ApiOriginFilter.java
+++ data/tdm/generator/src/main/java/io/swagger/api/ApiOriginFilter.java
@@ -1,3 +1,6 @@
+/*
+ *
+ */
 package io.swagger.api;
 
 import java.io.IOException;
@@ -9,23 +12,46 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ApiOriginFilter.
+ */
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-09-22T08:24:51.189Z[GMT]")
 public class ApiOriginFilter implements javax.servlet.Filter {
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.addHeader("Access-Control-Allow-Origin", "*");
-        res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-        res.addHeader("Access-Control-Allow-Headers", "Content-Type");
-        chain.doFilter(request, response);
-    }
 
-    @Override
-    public void destroy() {
-    }
+	/**
+	 * Destroy.
+	 */
+	@Override
+	public void destroy() {
+	}
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
+	/**
+	 * Do filter.
+	 *
+	 * @param request  the request
+	 * @param response the response
+	 * @param chain    the chain
+	 * @throws IOException      Signals that an I/O exception has occurred.
+	 * @throws ServletException the servlet exception
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		final HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("Access-Control-Allow-Origin", "*");
+		res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+		res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * Inits the.
+	 *
+	 * @param filterConfig the filter config
+	 * @throws ServletException the servlet exception
+	 */
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
 }
