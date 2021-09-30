7dffe3433a0f4ce052ffc38b284a43a190ae9fa6
Izabela Kulakowska <ikulakowska@microsoft.com>
2021-09-30 11:56:45 +0200

    Load data from static json files

--- coreservices/partsrelationshipservice/cd/test-data/generate-test-data.sh
+++ coreservices/partsrelationshipservice/cd/test-data/generate-test-data.sh
@@ -2,33 +2,26 @@
 
 set -euo pipefail
 
-purge_output_file=purge.txt.tmp
-json_data_file=data.json.tmp
-
-# Replace with TDM url once available
-TDM_URL=localhost:8080
-
-# Get relationships for a known business partner
-curl -f 'http://'$TDM_URL'/catena-x/tdm/1.0/prs/broker/PartRelationshipUpdateList' -H "accept: application/json" > $json_data_file
+# Purge data from PRS database
+echo 'DELETE * from public.part_relationship WHEN oneidmanufacturer IN ("CAXLBRHHQAJAIOZZ", "CAXLHNJURNRLPCZZ", "CAXLTHAJNAHZXGZZ", "CAXLZJVJEBYWYYZZ",
+"CAXSWPFTJQEVZNZZ", "CAXSZJVJEBYWYYZZ") OR parent_oneidmanufacturer IN ("CAXLBRHHQAJAIOZZ", "CAXLHNJURNRLPCZZ", "CAXLTHAJNAHZXGZZ", "CAXLZJVJEBYWYYZZ", "CAXSWPFTJQEVZNZZ", "CAXSZJVJEBYWYYZZ");'
+echo 'DELETE * from public.part_aspect WHEN oneidmanufacturer IN ("CAXLBRHHQAJAIOZZ", "CAXLHNJURNRLPCZZ", "CAXLTHAJNAHZXGZZ", "CAXLZJVJEBYWYYZZ", "CAXSWPFTJQEVZNZZ", "CAXSZJVJEBYWYYZZ");'
+echo 'DELETE * public.part_attribute WHEN oneidmanufacturer IN ("CAXLBRHHQAJAIOZZ", "CAXLHNJURNRLPCZZ", "CAXLTHAJNAHZXGZZ", "CAXLZJVJEBYWYYZZ", "CAXSWPFTJQEVZNZZ", "CAXSZJVJEBYWYYZZ");'
 
 # Generate SQL to load part_relationship data (parent-child relationships)
 echo 'COPY public.part_relationship (oneidmanufacturer, objectidmanufacturer, parent_oneidmanufacturer, parent_objectidmanufacturer, part_relationship_list_id, upload_date_time) FROM stdin CSV;'
-jq -r '(now | strftime("%Y-%m-%dT%H:%M:%S%z")) as $n | .[].relationships | .[].relationship | [.child.oneIDManufacturer, .child.objectIDManufacturer, .parent.oneIDManufacturer, .parent.objectIDManufacturer, "78F4BB1B-2EBB-418C-9C16-3E74BACCBEAC", $n] | @csv' $json_data_file
+jq -r '(now | strftime("%Y-%m-%dT%H:%M:%S%z")) as $n | .[].relationships | .[].relationship | [.child.oneIDManufacturer, .child.objectIDManufacturer, .parent
+.oneIDManufacturer, .parent.objectIDManufacturer, "78F4BB1B-2EBB-418C-9C16-3E74BACCBEAC", $n] | @csv' "PartRelationshipUpdateList.json"
 echo '\.'
-
-# Get part aspects for a known business partner
-curl -f 'http://'$TDM_URL'/catena-x/tdm/1.0/prs/broker/PartAspectUpdate' -H "accept: application/json" > $json_data_file
 
 # Generate SQL to load part_aspect data (aspect URLs)
 echo 'COPY public.part_aspect (name, oneidmanufacturer, objectidmanufacturer, url, effect_time, last_modified_time) FROM stdin CSV;'
-jq -r '(now | strftime("%Y-%m-%dT%H:%M:%S%z")) as $n | .[] | .part as $p | .aspects[] | [.name, $p.oneIDManufacturer, $p.objectIDManufacturer, .url, $n, $n] | @csv' $json_data_file
+jq -r '(now | strftime("%Y-%m-%dT%H:%M:%S%z")) as $n | .[] | .part as $p | .aspects[] | [.name, $p.oneIDManufacturer, $p.objectIDManufacturer, .url, $n, $n]
+| @csv' "PartAspectUpdate.json"
 echo '\.'
-
-# Get part attributes for a known business partner
-curl -f 'http://'$TDM_URL'/catena-x/tdm/1.0/prs/broker/PartTypeNameUpdate' -H "accept: application/json" > $json_data_file
 
 # Generate SQL to load part_attribute data (partTypeName field)
 echo 'COPY public.part_attribute (name, oneidmanufacturer, objectidmanufacturer, value, effect_time, last_modified_time) FROM stdin CSV;'
 jq -r '(now | strftime("%Y-%m-%dT%H:%M:%S%z")) as $n | .[] | .part as $p | ["partTypeName", $p.oneIDManufacturer, $p.objectIDManufacturer, .partTypeName, $n,
-$n] | @csv' $json_data_file
+ $n] | @csv' "PartTypeNameUpdate.json"
 echo '\.'
--- coreservices/partsrelationshipservice/cd/test-data/load-test-data.sh
+++ coreservices/partsrelationshipservice/cd/test-data/load-test-data.sh
@@ -6,19 +6,13 @@ echo "PostgreSQL host: $POSTGRES_HOST"
 echo "PostgreSQL database: $POSTGRES_DB"
 echo "PostgreSQL user: $POSTGRES_USER"
 
-shutdown_docker_compose() {
-    echo "Cleaning up..."
-    docker-compose -f $tdm_dock down  # in case a previous run failed in the middle
-}
-
 shutdown_on_error() {
-    shutdown_docker_compose
     exit 1
 }
 
 trap shutdown_on_error INT TERM ERR
 
+sql_data_file=data.sql.tmp
+
 ./generate-test-data.sh > $sql_data_file
 psql -v ON_ERROR_STOP=1 "host=$POSTGRES_HOST dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD" -f $sql_data_file
-
-shutdown_docker_compose
