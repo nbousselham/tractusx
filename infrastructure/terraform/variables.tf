31b033da2701f218f30dad72714d7b26c77c51e5
Ivan Muravlev <ivan.muravlev@t-systems.com>
2021-10-05 20:04:03 +0300

    Landscape dev008 creation. Landscape dev007 seems to be created not from terraform

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -77,7 +77,7 @@ variable "workspace_to_stage_map" {
     dev042  = "dev"
     dev005  = "dev"
     dev006  = "dev"
-    dev007  = "dev"
+    dev008  = "dev"
     int     = "int"    
     prod    = "prod"
   }
