398b6c907f66cc0b49b05b7d02e1633d9d008ee0
Marc Gomez <marcgomez@microsoft.com>
2021-11-09 10:31:17 +0100

    Prevent NPE if destination file does not exist

--- coreservices/partsrelationshipservice/connector/prs-connector-consumer/src/main/java/org/eclipse/dataspaceconnector/extensions/api/FileStatusChecker.java
+++ coreservices/partsrelationshipservice/connector/prs-connector-consumer/src/main/java/org/eclipse/dataspaceconnector/extensions/api/FileStatusChecker.java
@@ -3,6 +3,7 @@ package org.eclipse.dataspaceconnector.extensions.api;
 import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.eclipse.dataspaceconnector.spi.types.domain.transfer.*;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -16,19 +17,22 @@ public class FileStatusChecker implements StatusChecker {
 
     @Override
     public boolean isComplete(TransferProcess transferProcess, List<ProvisionedResource> list) {
+        File destinationFile = getDestinationFile(transferProcess);
+        monitor.info("File status checker: " + destinationFile.getAbsolutePath() + " :" + destinationFile.exists());
+        return destinationFile.exists();
+    }
+
+    private File getDestinationFile(TransferProcess transferProcess) {
         var destination = transferProcess.getDataRequest().getDataDestination();
         var destinationPath = Path.of(destination.getProperty("path"));
 
-        if (destinationPath.toFile().isDirectory()) {
+        File file = destinationPath.toFile();
+        if (file.exists() && file.isDirectory()) {
             var source = transferProcess.getDataRequest().getDataEntry().getCatalogEntry().getAddress();
             var sourceFileName = source.getProperty("filename");
-            destinationPath = Path.of(destinationPath.toString(), sourceFileName);
+            return Path.of(destinationPath.toString(), sourceFileName).toFile();
         }
 
-        boolean exists = destinationPath.toFile().exists();
-
-        monitor.info("File status checker: " + destinationPath + " :" + exists);
-
-        return exists;
+        return file;
     }
 }
