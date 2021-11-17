11182d2882163c93cfd89b5818e0b6a05d10f234
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-11-17 10:56:17 +0100

    .

--- coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/systemtest/ConnectorSystemTests.java
+++ coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/systemtest/ConnectorSystemTests.java
@@ -9,7 +9,6 @@
 //
 package net.catenax.prs.systemtest;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import net.catenax.prs.requests.PartsTreeByObjectIdRequest;
 import org.junit.jupiter.api.Tag;
@@ -24,8 +23,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
 import static net.catenax.prs.systemtest.SystemTestsBase.ASPECT_MATERIAL;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
@@ -43,21 +44,25 @@ import static org.awaitility.Awaitility.await;
 @Tag("SystemTests")
 public class ConnectorSystemTests {
 
-    private static final String baseURI = System.getProperty("ConnectorProviderBaseURI", "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com");
-    private static final String namespace = System.getProperty("ConnectorProviderK8sNamespace", "prs-connectors");
-    private static final String pod = System.getProperty("ConnectorProviderK8sPod", "prs-connector-provider-0");
+    private static final String consumerURI = System.getProperty("ConnectorConsumerURI",
+            "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com/prs-connector-consumer");
+    private static final String providerURI = System.getProperty("ConnectorProviderURI",
+            "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com/bmw/mtpdc/connector");
+    private static final String namespace = System.getProperty("ConnectorProviderK8sNamespace",
+            "prs-bmw");
+    private static final String pod = System.getProperty("ConnectorProviderK8sPod",
+            "prs-connector-provider-0");
     private static final String VEHICLE_ONEID = "CAXSWPFTJQEVZNZZ";
     private static final String VEHICLE_OBJECTID = "UVVZI9PKX5D37RFUB";
-    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    public void downloadFile() throws Exception {
+    public void downloadFile() {
 
         // Arrange
         var environment = System.getProperty("environment", "dev");
 
         // Temporarily hardcode the file path. It will change when adding several providers.
-        var fileWithExpectedOutput = "getPartsTreeByOneIdAndObjectId-dev-bmw-expected.json";
+        var fileWithExpectedOutput = format("getPartsTreeByOneIdAndObjectId-%s-bmw-expected.json", environment);
         var payload = getClass().getResourceAsStream(fileWithExpectedOutput);
 
         // Act
@@ -66,7 +71,7 @@ public class ConnectorSystemTests {
         var destFile = "/tmp/copy/dest/" + UUID.randomUUID();
         Map<String, Object> params = new HashMap<>();
         params.put("filename", "test-document");
-        params.put("connectorAddress", baseURI + "/prs-connector-provider");
+        params.put("connectorAddress", providerURI);
         params.put("destinationPath", destFile);
         params.put("partsTreeRequest", PartsTreeByObjectIdRequest.builder()
                 .oneIDManufacturer(VEHICLE_ONEID)
@@ -76,7 +81,7 @@ public class ConnectorSystemTests {
                 .depth(2)
                 .build());
 
-        RestAssured.baseURI = baseURI + "/prs-connector-consumer";
+        RestAssured.baseURI = consumerURI;
         var requestId =
                 given()
                         .contentType("application/json")
@@ -105,7 +110,7 @@ public class ConnectorSystemTests {
                                 .when(IGNORING_ARRAY_ORDER)
                                 .isEqualTo(expectedResult);
                     }
-                    exec.waitFor();
+                    assertThat(exec.waitFor()).isEqualTo(0);
                 });
     }
 
@@ -118,6 +123,7 @@ public class ConnectorSystemTests {
                 pod,
                 "--"));
         l.addAll(Arrays.asList(command));
+        System.out.println(l.stream().collect(Collectors.joining(" ")));
         return new ProcessBuilder()
                 .redirectError(ProcessBuilder.Redirect.INHERIT)
                 .command(l)
