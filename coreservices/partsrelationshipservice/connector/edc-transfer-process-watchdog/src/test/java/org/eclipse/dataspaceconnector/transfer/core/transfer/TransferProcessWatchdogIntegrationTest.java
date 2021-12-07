4bc9f76800e4298aa10b2f16b88ec757cd4bc5bd
Marc Gomez <marcgomez@microsoft.com>
2021-12-07 14:23:33 +0100

    Integration test for transfer process watchdog

---
+++ coreservices/partsrelationshipservice/connector/edc-transfer-process-watchdog/src/test/java/org/eclipse/dataspaceconnector/transfer/core/transfer/TransferProcessWatchdogIntegrationTest.java
@@ -0,0 +1,73 @@
+package org.eclipse.dataspaceconnector.transfer.core.transfer;
+
+import com.github.javafaker.Faker;
+import org.eclipse.dataspaceconnector.junit.launcher.EdcExtension;
+import org.eclipse.dataspaceconnector.spi.transfer.TransferProcessObservable;
+import org.eclipse.dataspaceconnector.spi.transfer.store.TransferProcessStore;
+import org.eclipse.dataspaceconnector.spi.types.domain.metadata.DataEntry;
+import org.eclipse.dataspaceconnector.spi.types.domain.transfer.DataRequest;
+import org.eclipse.dataspaceconnector.spi.types.domain.transfer.StatusChecker;
+import org.eclipse.dataspaceconnector.spi.types.domain.transfer.StatusCheckerRegistry;
+import org.eclipse.dataspaceconnector.spi.types.domain.transfer.TransferProcess;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Properties;
+
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.easymock.EasyMock.*;
+import static org.eclipse.dataspaceconnector.spi.types.domain.transfer.TransferProcess.Type.CONSUMER;
+import static org.eclipse.dataspaceconnector.spi.types.domain.transfer.TransferProcessStates.ERROR;
+import static org.eclipse.dataspaceconnector.spi.types.domain.transfer.TransferProcessStates.IN_PROGRESS;
+
+@ExtendWith(EdcExtension.class)
+public class TransferProcessWatchdogIntegrationTest {
+
+    Faker faker = new Faker();
+
+    @BeforeAll
+    public static void setProperties() {
+        Properties props = System.getProperties();
+        props.setProperty("edc.watchdog.timeout", "1");
+    }
+
+    @Test
+    public void cancelLongRunningProcess(TransferProcessStore transferProcessStore, StatusCheckerRegistry statusCheckerRegistry) throws InterruptedException {
+        // Arrange
+        DataRequest request = DataRequest.Builder.newInstance().protocol("ids-rest")
+                .dataEntry(DataEntry.Builder.newInstance().id(faker.lorem().characters()).build())
+                .connectorId(faker.lorem().characters())
+                .connectorAddress(faker.internet().url())
+                .destinationType(faker.lorem().word())
+                .managedResources(false)
+                .build();
+
+        StatusChecker statusChecker = mock(StatusChecker.class);
+        expect(statusChecker.isComplete(anyObject(TransferProcess.class), eq(emptyList()))).andReturn(false);
+        statusCheckerRegistry.register("XXX", statusChecker);
+
+        var process = TransferProcess.Builder.newInstance()
+                .id(faker.lorem().characters())
+                .dataRequest(request)
+                .type(CONSUMER)
+                .state(IN_PROGRESS.code())
+                .build();
+
+        // Act
+        transferProcessStore.update(process);
+        Thread.sleep(2000);
+
+        // Assert
+        assertThat(transferProcessStore.find(process.getId())).usingRecursiveComparison()
+                .ignoringFields("stateTimestamp", "stateCount")
+                .isEqualTo(TransferProcess.Builder.newInstance()
+                        .id("123")
+                        .dataRequest(request)
+                        .type(CONSUMER)
+                        .state(ERROR.code())
+                        .errorDetail("Timeout (1s)")
+                        .build());
+    }
+}
