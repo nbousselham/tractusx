88805bea25ce12d575325073b06421510871f9a2
Marc Gomez <marcgomez@microsoft.com>
2021-12-08 12:25:52 +0100

    Switch test to easymock

--- coreservices/partsrelationshipservice/connector/edc-transfer-process-watchdog/src/test/java/org/eclipse/dataspaceconnector/transfer/core/transfer/CancelLongRunningProcessesTest.java
+++ coreservices/partsrelationshipservice/connector/edc-transfer-process-watchdog/src/test/java/org/eclipse/dataspaceconnector/transfer/core/transfer/CancelLongRunningProcessesTest.java
@@ -1,26 +1,25 @@
 package org.eclipse.dataspaceconnector.transfer.core.transfer;
 
 import com.github.javafaker.Faker;
+import org.easymock.Capture;
+import org.easymock.EasyMockExtension;
+import org.easymock.Mock;
 import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.eclipse.dataspaceconnector.spi.transfer.store.TransferProcessStore;
 import org.eclipse.dataspaceconnector.spi.types.domain.transfer.TransferProcess;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static java.time.Instant.now;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.easymock.EasyMock.*;
 import static org.eclipse.dataspaceconnector.spi.types.domain.transfer.TransferProcessStates.ERROR;
 import static org.eclipse.dataspaceconnector.spi.types.domain.transfer.TransferProcessStates.IN_PROGRESS;
-import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(EasyMockExtension.class)
 class CancelLongRunningProcessesTest {
 
     public static final long STATE_TIMEOUT_MS = 20000;
@@ -32,8 +31,8 @@ class CancelLongRunningProcessesTest {
     Monitor monitor;
     @Mock
     TransferProcessStore transferProcessStore;
-    @Captor
-    ArgumentCaptor<TransferProcess> transferProcessCaptor;
+
+    Capture<TransferProcess> transferProcessCaptor = Capture.newInstance();
 
     CancelLongRunningProcesses sut;
 
@@ -59,14 +58,14 @@ class CancelLongRunningProcessesTest {
                 .stateTimestamp(now().minusSeconds(STATE_TIMEOUT_MS + 1).toEpochMilli())
                 .build();
 
-        when(transferProcessStore.nextForState(IN_PROGRESS.code(), BATCH_SIZE)).thenReturn(List.of(p1, p2));
-        doNothing().when(transferProcessStore).update(transferProcessCaptor.capture());
+        expect(transferProcessStore.nextForState(IN_PROGRESS.code(), BATCH_SIZE)).andReturn(List.of(p1, p2));
+        transferProcessStore.update(capture(transferProcessCaptor));
+        replay(transferProcessStore);
 
         // Act
         sut.run();
 
         // Assert
-        verify(transferProcessStore, times(1)).update(p2);
         var transferProcess = transferProcessCaptor.getValue();
         assertThat(transferProcess).usingRecursiveComparison().ignoringFields("stateCount", "stateTimestamp").isEqualTo(
                 TransferProcess.Builder.newInstance()
