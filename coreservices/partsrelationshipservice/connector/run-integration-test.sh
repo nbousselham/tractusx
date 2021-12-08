91d131014d86a9aead35e51896f5ca173620c235
Marc Gomez <marcgomez@microsoft.com>
2021-12-08 14:26:58 +0100

    Add integration test for failing PRS query

--- coreservices/partsrelationshipservice/connector/run-integration-test.sh
+++ coreservices/partsrelationshipservice/connector/run-integration-test.sh
@@ -8,5 +8,12 @@ if [ ! -f dev/local/cert.pfx ]; then
 fi
 
 export DOCKER_BUILDKIT=1
+# Successful parts tree retrieval IT
 docker-compose --profile connector build --build-arg PRS_EDC_PKG_USERNAME=$PRS_EDC_PKG_USERNAME --build-arg PRS_EDC_PKG_PASSWORD=$PRS_EDC_PKG_PASSWORD
+IT_SCRIPT=connector-integration-test.sh \
 docker-compose --profile connector --profile prs up --exit-code-from=connector-integration-test --abort-on-container-exit
+
+# Failing parts tree retrieval IT (prs not available)
+docker-compose --profile connector build --build-arg PRS_EDC_PKG_USERNAME=$PRS_EDC_PKG_USERNAME --build-arg PRS_EDC_PKG_PASSWORD=$PRS_EDC_PKG_PASSWORD
+IT_SCRIPT=connector-api-failure-integration-test.sh \
+docker-compose --profile connector up --exit-code-from=connector-integration-test --abort-on-container-exit
