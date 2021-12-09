e6cb03645dfed06bd093839903be685947284e82
Janine Semmler <contact@janemcbrain.com>
2021-12-09 09:29:31 +0100

    Feature/CATX-A1-ODT@99 Add Material UI lib (#339)
    * https://jira.catena-x.net/browse/A1ODT-99 add Material UI lib
    
    * https://jira.catena-x.net/browse/A1ODT-99 add docu for theming

--- devops-tooling/dashboard/src/App.tsx
+++ devops-tooling/dashboard/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import './App.scss';
 import Dashboard from './components/Dashboard/Dashboard';
+import ThemeProvider from '@mui/system/ThemeProvider'
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import theme from './Theme';
 
 function App() {
   return (
-    <div className="app">
-      <header className="app__header">
-        DevOps Tooling
-      </header>
-      <Dashboard />
-  </div>
+    <ThemeProvider theme={theme}>
+      <Grid container direction="column">
+        <Grid container justifyContent="center" sx={{p: 2}}>
+          <span>DevOps Tooling</span>
+          <Button variant="contained" color="primary">Just a button</Button>
+        </Grid>
+        <Dashboard />
+      </Grid>
+    </ThemeProvider >
   );
 }
 
---
+++ devops-tooling/dashboard/src/Theme.ts
@@ -0,0 +1,16 @@
+//GENERAL DOC: https://mui.com/customization/theming/
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#b3cb2d'
+    },
+    secondary: {
+      main: '#ffa600'
+    },
+  }
+});
+
+export default theme;
