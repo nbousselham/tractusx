9840764d25a7caa5e344605555e250b802dc084f
= <=>
2021-12-10 11:30:48 +0100

    Added user types

--- devops-tooling/dashboard/src/components/Login/Login.tsx
+++ devops-tooling/dashboard/src/components/Login/Login.tsx
@@ -3,7 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-//import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
@@ -24,10 +23,19 @@ export default function Login() {
     const data = new FormData(event.currentTarget);
    
     const username =data.get('username');
-    if(typeof username === 'string') {
-      auth.signin(username,()=> {console.log("asd"); navigate(from, { replace: true });});
+    const password  = data.get('password');
+
+
+    if(typeof username !== 'string' || typeof password !== 'string' ) {
+      return ;
     }
-   
+
+    if((username === 'admin' && password === 'admin')  || (username === 'user' && password === 'user')) {
+     auth.signin(username,()=> {console.log("asd"); navigate(from, { replace: true });});
+    }
+    //error handling here
+    
+    
   };
 
   return (
