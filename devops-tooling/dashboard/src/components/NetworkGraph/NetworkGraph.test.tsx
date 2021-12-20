ef9c21ff68e832b1ff477a24406b5860bb55884f
dhiraj-wakchaure <93985179+dhiraj-wakchaure@users.noreply.github.com>
2021-12-20 13:40:48 +0100

    CATX-A1-ODT-114 (#368)
    * CATX-A1-ODT-114
    
    added test case for dashboard to test diffrent users
    
    fixed networkgraph test cases
    
    * CATX-A1-ODT-114
    
    added dashboard test cases for diffrent users
    fixed netwrokgraph test cases
    
    Co-authored-by: = <=>

--- devops-tooling/dashboard/src/components/NetworkGraph/NetworkGraph.test.tsx
+++ devops-tooling/dashboard/src/components/NetworkGraph/NetworkGraph.test.tsx
@@ -5,12 +5,13 @@ import data from '../../pages/Dashboard/data.json';
 
 const nodesData = data.nodes.map((d: any) => Object.assign({}, d));
 const linksData = data.links;
+const parentSize = {width:100,height:100};
 
 describe("NetworkGraph rendering", () => {
 
   test('it renders connectors', () => {
 
-    render(<NetworkGraph  nodes={nodesData} links={linksData}/>);
+    render(<NetworkGraph  nodes={nodesData} links={linksData} parentSize={parentSize} />);
     const connectorElement = screen.getByText('Connector 1');
     expect(connectorElement).toBeInTheDocument();
     const connectorElement2 = screen.getByText('Connector 2');
@@ -19,7 +20,7 @@ describe("NetworkGraph rendering", () => {
   });
 
   test('it matches snapshots',()=> {
-    let graphRender =  render(<NetworkGraph  nodes={nodesData} links={linksData}/>);
+    let graphRender =  render(<NetworkGraph  nodes={nodesData} links={linksData} parentSize={parentSize} />);
     expect(graphRender).toMatchSnapshot();
   })
 
--- devops-tooling/dashboard/src/pages/Dashboard/Dashboard.test.tsx
+++ devops-tooling/dashboard/src/pages/Dashboard/Dashboard.test.tsx
@@ -1,16 +1,30 @@
-
-
 import { render, screen } from '@testing-library/react';
 import Dashboard from './Dashboard'
+import useAuth from '../../Auth/useAuth';
 
 jest.mock('../../components/NetworkGraph/NetworkGraph', () => () => (<div>Hello World</div>));
+jest.mock("../../Auth/useAuth");
 
 describe('shallow rendering <Dashboard />', () => {
 
   test('svg div', () => {
+    useAuth.mockReturnValue({user:"user"})
     render(<Dashboard />);
     // find svg element
     const dashboardElement = screen.getByTestId('dashboard');
     expect(dashboardElement).toBeInTheDocument();
   });
+
+  test("snapshot for user",()=>{
+    useAuth.mockReturnValue({user:"user"})
+    const dashBoard = render(<Dashboard />);
+    expect(dashBoard).toMatchSnapshot()
+  })
+
+  test("snapshot for admin",()=>{
+    useAuth.mockReturnValue({user:"admin"})
+    const dashBoard = render(<Dashboard />);
+    expect(dashBoard).toMatchSnapshot()
+  })
+
 })
