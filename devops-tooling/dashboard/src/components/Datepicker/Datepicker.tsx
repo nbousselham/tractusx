1107a98620abb00d232b241bc6b5a6f9e77df063
JaneMcBrain <contact@janemcbrain.com>
2022-02-02 15:44:57 +0100

    error fixes

--- devops-tooling/dashboard/src/components/Datepicker/Datepicker.tsx
+++ devops-tooling/dashboard/src/components/Datepicker/Datepicker.tsx
@@ -1,11 +1,9 @@
-import React from "react";
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 import TextField from '@mui/material/TextField';
 
-
-export default function Datepicker(props){
+export default function Datepicker(props) {
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <DatePicker
@@ -13,11 +11,10 @@ export default function Datepicker(props){
         value={props.value}
         minDate={props.minDate}
         maxDate={props.maxDate}
-        onChange={(newValue) => {props.setValue(newValue);}}
+        onChange={(newValue) => { props.setValue(newValue); }}
         renderInput={(params) => <TextField fullWidth={props.fullWidth} {...params} />}
-        views={["year","month","day"]}
+        views={['year', 'month', 'day']}
       />
     </LocalizationProvider>
-  )
-
+  );
 }
