0e745420f8fecef32e5afdc5bcd9ea5e419d7894
JaneMcBrain <contact@janemcbrain.com>
2022-02-08 09:16:11 +0100

    update API endpoint for registry and semantic hub

--- portal/code/tractus-x-portal/src/components/digitaltwins/data.ts
+++ portal/code/tractus-x-portal/src/components/digitaltwins/data.ts
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-const TWIN_URL = `${process.env.REACT_APP_SEMANTIC_SERVICE_LAYER_URL}twins`;
+const TWIN_URL = `${process.env.REACT_APP_SEMANTIC_SERVICE_LAYER_URL}registry/shell-descriptors`;
 
 interface httpEndpoint {
   id: string,
--- portal/code/tractus-x-portal/src/components/semantics/data.ts
+++ portal/code/tractus-x-portal/src/components/semantics/data.ts
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-const MODEL_URL = `${process.env.REACT_APP_SEMANTIC_SERVICE_LAYER_URL}models`;
+const MODEL_URL = `${process.env.REACT_APP_SEMANTIC_SERVICE_LAYER_URL}api/v1/models`;
 
 export enum Status {
   Draft = "DRAFT",
