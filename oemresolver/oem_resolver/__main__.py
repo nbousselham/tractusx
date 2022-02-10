752ea7626ada77f01b3bb8de7bd4f9f90800df75
Felix Gerbig <felix.gerbig@bmw.de>
2022-02-10 18:11:06 +0100

    Added kubernetes support

--- oemresolver/oem_resolver/__main__.py
+++ oemresolver/oem_resolver/__main__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import connexion
 from dotenv import load_dotenv
 
--- oemresolver/oem_resolver/controllers/authorization_controller.py
+++ oemresolver/oem_resolver/controllers/authorization_controller.py
@@ -1,35 +1,45 @@
-from oem_resolver.util import _current_timestamp
-from typing import List
-from jose import JWTError, jwt
-from werkzeug.exceptions import Unauthorized
+#from oem_resolver.util import _current_timestamp
+#from typing import List
+# from jose import JWTError, jwt
+# from werkzeug.exceptions import Unauthorized
 from os import environ
+from connexion.exceptions import OAuthProblem
+import json
 
+API_KEY = json.loads(environ.get("API_KEY"))
 
-JWT_ISSUER = environ.get('JWT_ISSUER')
-JWT_SECRET = environ.get('JWT_SECRET')
-JWT_LIFETIME_SECONDS = int(environ.get('JWT_LIFETIME_SECONDS'))
-JWT_ALGORITHM = environ.get('JWT_ALGORITHM')
+def apikey_auth(token, required_scopes):
+    info = API_KEY.get(token, None)
+    if not info:
+        raise OAuthProblem('Invalid token')
 
-"""
-controller generated to handled auth operation described at:
-https://connexion.readthedocs.io/en/latest/security.html
-"""
-def check_bearerAuth(token):
-    try:
-        return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-    except JWTError as e:
-        raise Unauthorized from e
+    return info
+
+# JWT_ISSUER = environ.get('JWT_ISSUER')
+# JWT_SECRET = environ.get('JWT_SECRET')
+# JWT_LIFETIME_SECONDS = int(environ.get('JWT_LIFETIME_SECONDS'))
+# JWT_ALGORITHM = environ.get('JWT_ALGORITHM')
+
+# """
+# controller generated to handled auth operation described at:
+# https://connexion.readthedocs.io/en/latest/security.html
+# """
+# def check_bearerAuth(token):
+#     try:
+#         return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+#     except JWTError as e:
+#         raise Unauthorized from e
         
-def generate_token(user_id):
+# def generate_token(user_id):
     
 
-    timestamp = _current_timestamp()
-    payload = {
-        "iss": JWT_ISSUER,
-        "iat": int(timestamp),
-        "exp": int(timestamp + JWT_LIFETIME_SECONDS),
-        "sub": str(user_id),
-    }
+#     timestamp = _current_timestamp()
+#     payload = {
+#         "iss": JWT_ISSUER,
+#         "iat": int(timestamp),
+#         "exp": int(timestamp + JWT_LIFETIME_SECONDS),
+#         "sub": str(user_id),
+#     }
 
-    return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+#     return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
