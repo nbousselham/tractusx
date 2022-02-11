d4442bc74306c5c2747ccf97d8f8ade444770139
JaneMcBrain <contact@janemcbrain.com>
2022-02-11 17:02:53 +0100

    upate Digital Twin detail page

--- portal/code/tractus-x-portal/src/components/digitaltwins/DigitalTwinDetail.tsx
+++ portal/code/tractus-x-portal/src/components/digitaltwins/DigitalTwinDetail.tsx
@@ -9,15 +9,14 @@ import { DigitalTwin } from "./interfaces";
 
 export function DigitalTwinDetail(props){
   const id = props.match.params.id;
-  const [twin, setTwin] = useState<DigitalTwin | any>(null);
-  const [error, setError] = useState(null);
+  const [twin, setTwin] = useState<DigitalTwin>();
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    getTwinById(id).then(twin => { 
-      setTwin(twin);},
-      error => {
-        setError(error.message);
-      })
+    getTwinById(id).then(
+      res => {setTwin(res); console.log(res)},
+      error => setError(error.message)
+    )
   }, [id])
 
 
@@ -26,46 +25,75 @@ export function DigitalTwinDetail(props){
       <BackLink history={props.history} />
       {twin ?
         <div className='m5 p20 bgpanel flex40 br4 bsdatacatalog'>
-          <h2 className='fs24 bold'>{twin.description}</h2>
+          <h2 className='fs24 bold'>{twin.idShort || twin.identification}</h2>
           <div className='mt20 mb30'>
-            <DescriptionList title="Manufacturer" description={twin.manufacturer} />
+            <DescriptionList title="Description" description={twin.description[0].text} />
+            {twin.administration &&
+              <>
+                <h3 className="fs20 bold">Administration</h3>
+                <DescriptionList title="Version" description={twin.administration.version} />
+                <DescriptionList title="Revision" description={twin.administration.revision} />
+              </>
+            }
+            {twin.submodelDescriptors && <DescriptionList title="Submodel Endpoints Count" description={twin.submodelDescriptors.length}/>}
             <div className='mt20'>
-            <h3 className="fs20 bold">Local Identifiers</h3>
-            {twin.localIdentifiers.map(identifier => (
-              <div className='mt20'>
-                <DescriptionList title="Key" description={identifier.key}/>
-                <DescriptionList title="Value" description={identifier.value}/>
-              </div>
-            ))}
+              <h3 className="fs20 bold">Endpoints</h3>
+              {twin.endpoints && twin.endpoints.map(twinEp => (
+                <>
+                  <DescriptionList title="Interface" description={twinEp.interface}/>
+                  <dl>
+                    <dt className='dib minw150 fs14 fggrey'>Adress</dt>
+                    <dd className='fs14 fg5a dib'>
+                      <Link className="mr20" to={{
+                        pathname: `/home/semanticmodel/${twinEp.protocolInformation.endpointAddress}`
+                      }}>Endpoint Adress</Link>
+                    </dd>
+                  </dl>
+                  <DescriptionList title="Protocol" description={twinEp.protocolInformation.endpointProtocol}/>
+                  <DescriptionList title="Protocol Version" description={twinEp.protocolInformation.endpointProtocolVersion}/>
+                </>
+              ))}
             </div>
-            <h3 className='fs20 bold mt20'>Aspects</h3>
-            {twin.aspects.map( (aspect, index) => (
-              <div key={aspect.id} className={`mb15 mt25 bbss fgtab pb20 ${index === (twin.aspects.length -1) ? '' : 'bbw1'}`}>
-                <dl>
-                  <h4 className='dib minw150 fs14'>Model Reference URN</h4>
-                  <dd className='fs14 fg5a dib'>
-                    <Link className="ml25" to={{
-                      pathname: `/home/semanticmodel/${aspect.modelReference.urn.replace("#","%23")}`,
-                      state: aspect.modelReference.urn
-                    }}>{aspect.modelReference.urn}</Link>
-                  </dd>
-                </dl>
-                <h4 className="dib mt20 fs14">Endpoints</h4>
-                {aspect.httpEndpoints.map(httpEp => (
-                  <div key={httpEp.id} className="ml25 mt10">
-                    <dl>
-                      <dt className='dib minw150 fs14 fggrey'>URL</dt>
-                      <dd className='fs14 fg5a dib'>
-                        <Link className="mr20" to={{
-                          pathname: `/home/aspect/${httpEp.url}`
-                        }}>Aspect IDS Connector URL</Link>
-                      </dd>
-                    </dl>
-                    <DescriptionList title="Method" description={httpEp.method}/>
-                  </div>
-                ))}
-              </div>
-            ))}
+            <div className='mt20'>
+              <h3 className="fs20 bold">Submodel Descriptors</h3>
+              {twin.submodelDescriptors.map(submodel => (
+                <div key={submodel.identification} className='mt20'>
+                  <DescriptionList title="Name" description={submodel.idShort}/>
+                  <DescriptionList title="Description" description={submodel.description[0]}/>
+                  <DescriptionList title="Endpoints Count" description={submodel.endpoints.length}/>
+                  <dl>
+                    <dt className='dib minw150 fs14 fggrey'>Semantic ID</dt>
+                    <dd className='fs14 fg5a dib'>
+                      <Link
+                        className="ml25"
+                        to={{
+                          pathname: `/home/semanticmodel/${submodel.semanticId.value[0]}`,
+                          state: submodel.semanticId.value[0]
+                        }}
+                      >
+                        {submodel.semanticId.value[0]}
+                      </Link>
+                    </dd>
+                  </dl>
+                  <h4 className="dib mt20 fs14">Endpoints</h4>
+                  {submodel.endpoints.map(endpoint => (
+                    <div key={endpoint.interface} className="ml25 mt10">
+                      <DescriptionList title="Interface" description={endpoint.interface}/>
+                      <dl>
+                        <dt className='dib minw150 fs14 fggrey'>Adress</dt>
+                        <dd className='fs14 fg5a dib'>
+                          <Link className="mr20" to={{
+                            pathname: `/home/aspect/${endpoint.protocolInformation.endpointAddress}`
+                          }}>Aspect IDS Connector URL</Link>
+                        </dd>
+                      </dl>
+                      <DescriptionList title="Protocol" description={endpoint.protocolInformation.endpointProtocol}/>
+                      <DescriptionList title="Protocol Version" description={endpoint.protocolInformation.endpointProtocolVersion}/>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
           </div>
         </div> :
         <Loading />
