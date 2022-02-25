dac79540ee2ac919c43f0042fcfea2ad1f3fe776
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2022-02-25 14:33:07 +0100

    https://jira.catena-x.net/browse/A1SLDT-130 lacking headers.

--- semantics/framework/src/main/java/net/catenax/semantics/framework/BaseAdapter.java
+++ semantics/framework/src/main/java/net/catenax/semantics/framework/BaseAdapter.java
@@ -1,6 +1,11 @@
-/**
- * base class for backend adapters
- */
+/*
+Copyright (c) 2021-2022 T-Systems International GmbH (Catena-X Consortium)
+See the AUTHORS file(s) distributed with this work for additional
+information regarding authorship.
+
+See the LICENSE file(s) distributed with this work for
+additional information regarding license terms.
+*/
 package net.catenax.semantics.framework;
 
 import lombok.RequiredArgsConstructor;
